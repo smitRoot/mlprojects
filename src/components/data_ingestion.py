7597fb6dc31cf8286d5a77825bfc590fb55e4d09
Smit Patel <160984002+smitRoot@users.noreply.github.com>
2024-11-07 05:07:38 -0600

    Dat transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses  import dataclass
 
+from src.components.data_transformation import DataTransformation
+
+from src.components.data_transformation import DataTransformationConfig
 
 ## if you have only variable then data class is fine but if you have funcstion &.. sugeestion would be init 
 @dataclass
@@ -45,9 +48,12 @@ class DataIngestion:
         
 if __name__=="__main__": 
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 
-    
+     
 
